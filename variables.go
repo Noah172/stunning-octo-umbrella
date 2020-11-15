18972765e8bca8f2635ee4b7d309ba0dae946d6d
Noah172 <jsebastian.garcia1995@gmail.com>
2020-11-14 19:00:43 -0500

    starting with the variables

---
+++ variables.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int
+	x = 23
+	// las dos lineas anteriores se puede resumir en 'x := 23'
+
+	fmt.Println("la variable x es", x, "y es un dato tipo int")
+}
